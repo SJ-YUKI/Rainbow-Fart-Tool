c29dc719477382a5cadeee51de9a568e7d10d34f
水鏡 <67570185+SJ-YUKI@users.noreply.github.com>
2020-08-15 18:25:11 +0800

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,3 +1,5 @@
+debugger;
+
 var x;
 
 function input() {
@@ -24,15 +26,30 @@ function girl() {
 
 }
 
-function finish(){
+function finish() {
 
     var y = document.getElementById("Nam").value;
 
-    if(x == 1){
+    var g = ['是我的仙女，我想给你从清晨点赞到日暮，从北极跨越到南极。你的可爱，你的音容笑貌，让我中毒一般上瘾。','今日份的美貌也正常营业啦！','这样的美貌是真实存在的吗？！','是相机也无法承载的美貌啊！','小仙女是直男收割机！','仙子下凡辛苦了！',',有人说你不好看，我二话不说给他买了只导盲犬！','又在持靓行凶了!'];
 
-        document.getElementById("re").innerHTML = "因为" + y + "你是我的仙女，所以我想给你从清晨点赞到日暮，从北极跨越到南极。你的可爱，你的音容笑貌，让我中毒一般上瘾。我喜欢你。"
+    var b = ['今天也是宇宙独一无二的帅气，与众不同的帅气，表里如一的帅气，迷倒万千少女的帅气，说了再见也还是挥之不去的帅气，世界第一未解之谜，万千科学家用尽一生，所有的科学道理也解释不出的帅气。 ','今日份的帅气也正常营业啦！','这样的帅气是真实存在的吗？！','是相机也无法承载的帅气啊！','真是英俊潇洒、风流倜傥、玉树临风、神勇威武、天下无敌、宇内第一无与伦比、谦虚好学、...简直是前不见古人后不见来者!','有人说你不好看，我二话不说给他买了只导盲犬！'];
+    
+    if (x == 1) {
+
+        var i = Math.floor(Math.random() * g.length + 1)-1;
+
+        document.getElementById("re").innerHTML = y + g[i];
 
     }
+
+    else {
+
+        var i = Math.floor(Math.random() * b.length + 1)-1;
+
+        document.getElementById("re").innerHTML = y + b[i];
+
+    }
+}
 
     else{
 
